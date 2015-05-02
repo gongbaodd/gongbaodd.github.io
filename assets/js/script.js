c419cf3b7185c078752ab8fee6cd9c7d62b8265c
gongbaodd <gongbaodd@gmail.com>
2015-05-02 23:26:10 +0800

    怎么回事儿

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,15 +130,15 @@ function afterPjax() {
   });
 
   // 多说
-    var duoshuoQuery = {short_name:"gongbaodd"};
-    function check() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0]
-		 || document.getElementsByTagName('body')[0]).appendChild(ds);
-	}check();
+//    var duoshuoQuery = {short_name:"gongbaodd"};
+//    function check() {
+//		var ds = document.createElement('script');
+//		ds.type = 'text/javascript';ds.async = true;
+//		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+//		ds.charset = 'UTF-8';
+//		(document.getElementsByTagName('head')[0]
+//		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+//	}check();
 //  var ds_loaded = false,
 //      top = $('#disqus_thread').offset().top;
 //      identifier = $('#post__title').data('identifier');
@@ -156,5 +156,5 @@ function afterPjax() {
 //      ds_loaded = true;
 //    }
 //  }check();
-  container.scroll(check);
+//  container.scroll(check);
 }afterPjax();
