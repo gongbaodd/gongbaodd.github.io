6924de852722be12bff8b63248333bbb3dd82f9c
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-05 19:32:30 +0800

    ✨ Feat: Add github ribbon

--- config/plugins/transformer_remark.js
+++ config/plugins/transformer_remark.js
@@ -1,30 +1,54 @@
+const githubRibbon = [
+  {
+    resolve: "gatsby-plugin-github-ribbon",
+    options: {
+      project: "https://github.com/gongbaodd/gongbaodd.github.io",
+      color: "gray",
+      position: "right",
+    },
+  },
+];
+
+const images = [
+  {
+    resolve: "gatsby-remark-images",
+    options: {
+      maxWidth: 590,
+    },
+  },
+];
+
+const iframe = [
+  {
+    resolve: "gatsby-remark-responsive-iframe",
+    options: {
+      wrapperStyle: "margin-bottom: 1.0725rem",
+    },
+  },
+];
+
+const prismjs = [
+  {
+    resolve: "gatsby-remark-prismjs",
+    options: {
+      classPrefix: "language-",
+      showLineNumbers: true,
+    },
+  },
+];
+
 module.exports = [
   {
     resolve: "gatsby-transformer-remark",
     options: {
       pedantic: true,
       plugins: [
-        {
-          resolve: "gatsby-remark-images",
-          options: {
-            maxWidth: 590,
-          },
-        },
-        {
-          resolve: "gatsby-remark-responsive-iframe",
-          options: {
-            wrapperStyle: "margin-bottom: 1.0725rem",
-          },
-        },
+        ...githubRibbon,
+        ...images,
+        ...iframe,
+        ...prismjs,
         "gatsby-remark-copy-linked-files",
         "gatsby-remark-smartypants",
-        {
-          resolve: "gatsby-remark-prismjs",
-          options: {
-            classPrefix: "language-",
-            showLineNumbers: true,
-          },
-        },
       ],
     },
   },
