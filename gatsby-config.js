09eaefe4d5548b51cb49b3c39a37baa68cbf7dd4
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-05 18:23:38 +0800

    ✨ Feat: Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-159827550-1",
       },
     },
     // "gatsby-plugin-feed",
