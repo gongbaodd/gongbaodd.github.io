b8de8f7ad7cbec1a34ff15d238521be9d6840138
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-17 14:07:39 +0800

    ✨ Feat: remove cypress

--- cypress/integration/posts.feature
+++
@@ -1,5 +0,0 @@
-Feature: View posts
-
-Scenario: Opening the blog
-    Given I open the blog url
-    Then I see "宫不上叔" in the title
--- cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const { default: cucumber } = require("cypress-cucumber-preprocessor");
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
-
-module.exports = on => {
-  on("file:preprocessor", cucumber());
-  on("file:preprocessor", cypressTypeScriptPreprocessor);
-};
