4685a2eb90a4cea9c5aad3bbd3699b5bb49b3a1f
Travis CI User <travis@example.org>
2020-07-04 03:27:58 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "js/webpack-runtime.c82d824447ebc59d984b.js"
+    "url": "js/webpack-runtime.231378308243941c75b9.js"
   },
   {
     "url": "js/framework.4732038a8fedfb595f9d.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5aecfda4ee02248b3f7c2a6b7a427888"
+    "revision": "2cc69e8832eb04e20864274646954a97"
   },
   {
     "url": "js/component---cache-caches-gatsby-plugin-offline-app-shell-js.16d83d4f7361846e9539.js"
