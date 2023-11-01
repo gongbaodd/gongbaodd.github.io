704472e78544a50a76c223712a1c212ad2b5a241
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-01 19:07:10 +0800

    rename mix CH-EN

--- scripts/non-english-name.py
+++ scripts/non-english-name.py
@@ -1,4 +1,5 @@
 import os
+import re
 import string
 
 def contains_non_english_characters(text):
@@ -18,8 +19,25 @@ def find_files_with_non_english_characters(directory):
 
     return non_english_files
 
+def replace_non_english_with_hyphen(text):
+    # Use regex to match non-English characters
+    non_english_pattern = re.compile(r'[^(a-z|A-Z|0-9)\s.]')
+    # Replace non-English characters with hyphens
+    modified_text = re.sub(non_english_pattern, '-', text)
+    # Replace multiple consecutive hyphens with a single hyphen
+    modified_text = re.sub(r'-+', '-', modified_text)
+    # Remove leading and trailing hyphens
+    modified_text = modified_text.strip('-')
+    return modified_text
+
+
 directory_path = './_docs'
 non_english_files = find_files_with_non_english_characters(directory_path)
 
 for file in non_english_files:
-    print(file)
+    filename = os.path.basename(file)
+    name, ext = os.path.splitext(filename)
+    print("ext:" + ext)
+    print(name)
+    new_name = replace_non_english_with_hyphen(name)
+    if new_name : print(new_name + ext)
