40a7f5db671d92995d61aabddceafef8be8d351c
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-16 22:02:28 +0800

    Add echo statement to generate.sh script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,6 +3,7 @@ if [ ! -d "$GENERATOR_DIR" ]; then
     git clone https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 fi
 cd $GENERATOR_DIR
+echo "entering ${GENERATOR_DIR}"
 git pull
 rm -Rf ./src/content/blog/
 cp -Rf ../_docs ./src/content/blog
