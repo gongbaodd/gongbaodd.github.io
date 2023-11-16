84639f1479ba3d70bcb02a43816fa08b6a6b4b1f
gongbaodd@ProArt <gongbaodd@qq.com>
2023-11-16 21:49:27 +0800

    Refactor generate.sh script to clone and pull repository instead of deleting and copying

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,10 @@
-git clone https://github.com/gongbaodd/blog.gongbushang.com.git generator
-rm -Rf generator/src/content/blog/
-cp -Rf _docs generator/src/content/blog
-cd generator
+GENERATOR_DIR=./generator
+if [ ! -d "$GENERATOR_DIR" ]; then
+    git clone https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
+fi
+cd $GENERATOR_DIR
+git pull
+rm -Rf ./src/content/blog/
+cp -Rf ../_docs ./src/content/blog
 npm install
 npm run build
