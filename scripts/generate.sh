de2657c1d151552c04de230a49a3e5a1c0cbde6f
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2025-07-20 22:46:26 +0300

    change generate command

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,9 +1,7 @@
 GENERATOR_DIR=./generator
 rm -Rf $GENERATOR_DIR
-git clone https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
+git clone --recursive https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 cd $GENERATOR_DIR
 git pull
-rm -Rf ./src/content/blog/
-cp -Rf ../_docs ./src/content/blog
 yarn
 yarn build
