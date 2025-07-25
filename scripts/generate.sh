6e56f2543b5d95d3782da76394f5efc37b000f5b
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-25 15:29:47 +0300

    close the latest submodules

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,6 @@
 GENERATOR_DIR=./generator
 rm -Rf $GENERATOR_DIR
-git clone --recursive https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
+git clone  --recurse-submodules https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 cd $GENERATOR_DIR
 git pull
 pnpm install
