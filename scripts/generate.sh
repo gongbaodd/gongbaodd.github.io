1188e0270f576cd87ab4ff8e2e0ae43382f619ac
gongbaodd@ProArt <gongbaodd@qq.com>
2025-08-22 17:43:58 +0300

    use npm instead of pnpm

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,5 +7,5 @@ pnpm -v
 git clone  --recurse-submodules https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 cd $GENERATOR_DIR
 git pull
-pnpm install
-pnpm run build
+npm install
+npm run build
