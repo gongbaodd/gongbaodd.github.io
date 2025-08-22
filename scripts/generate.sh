e23775a56e0e9e0d96d1bffb4d8fc4172f504a1b
gongbaodd@ProArt <gongbaodd@qq.com>
2025-08-22 17:35:26 +0300

    use latest pnpm

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,6 +3,7 @@ rm -Rf $GENERATOR_DIR
 git clone  --recurse-submodules https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 cd $GENERATOR_DIR
 git pull
+npm install -g pnpm
 pnpm -v
 pnpm install
 pnpm run build
