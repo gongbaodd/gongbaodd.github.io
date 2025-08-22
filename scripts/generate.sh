2f42f3568a80c5ec0ba43a0ee74f52a0ec874d7e
gongbaodd@ProArt <gongbaodd@qq.com>
2025-08-22 17:49:14 +0300

    --legacy-peer-deps

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,5 +7,5 @@ pnpm -v
 git clone  --recurse-submodules https://github.com/gongbaodd/blog.gongbushang.com.git $GENERATOR_DIR
 cd $GENERATOR_DIR
 git pull
-npm install
+npm install --legacy-peer-deps
 npm run build
